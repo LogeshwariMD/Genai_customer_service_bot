e280d215c282ae2861144321bdf59c2ca68b47f4
Logeshwari <127845610+LogeshwariMD@users.noreply.github.com>
2025-09-24 19:36:39 +0530

    Add files via upload

---
+++ llama3/app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from langchain_ollama import OllamaLLM
+ollama = OllamaLLM(model="llama3")
+st.title("💬 llama3 Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "How can I help you?"}
+    ]
+
+
+for msg in st.session_state.messages:
+    st.chat_message(
+        msg["role"], avatar="🧑‍💻" if msg["role"] == "user" else "🤖"
+    ).write(msg["content"])
+
+
+
+def generate_response():
+    messages = [
+        {"role": msg["role"], "content": msg["content"]}
+        for msg in st.session_state.messages
+    ]
+    prompt = messages[-1]["content"]  # Get the last user message as the input
+    response = ollama.stream(input=prompt, messages=messages)
+    full_response = ""
+    for token in response:
+        full_response += token
+        yield token
+    st.session_state["full_message"] = full_response
+
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user", avatar="🧑‍💻").write(prompt)
+
+    with st.chat_message("assistant", avatar="🤖"):
+        response = st.write_stream(generate_response())
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": st.session_state["full_message"]}
+    )
